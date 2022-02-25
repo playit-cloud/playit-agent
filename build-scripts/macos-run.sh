7720d889f186856d550e107951c4c40ff4c3fc7c
Patrick Lorio <dev@plorio.com>
2022-02-25 09:22:42 -0800

    Set config and logs path on macos app

--- build-scripts/macos-run.sh
+++ build-scripts/macos-run.sh
@@ -2,13 +2,22 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+mkdir -p ~/.config/playit
+mkdir -p ~/Library/Logs/playit
+
+cd ~/.config/playit
+CONFIG_PATH=$(pwd)
+
+cd ~/Library/Logs/playit
+LOGS_PATH=$(pwd)
+
 if [[ $(uname -p) == 'arm' ]]; then
   osascript -e "tell app \"Terminal\"
-    do script \" ${SCRIPT_DIR}/agent-m1\"
+    do script \" ${SCRIPT_DIR}/agent-m1 --config-file=${CONFIG_PATH}/playit.toml --log-folder=${LOGS_PATH}/playit\"
   end tell"
 else
   osascript -e "tell app \"Terminal\"
-    do script \" ${SCRIPT_DIR}/agent-intel\"
+    do script \" ${SCRIPT_DIR}/agent-intel --config-file=${CONFIG_PATH}/playit.toml --log-folder=${LOGS_PATH}/playit\"
   end tell"
 fi
 
