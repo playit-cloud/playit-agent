6ba86ad3050a704eda91149a82335e8b50f49c42
Jay Jackson <jj@jj.is-a.dev>
2022-03-31 19:14:12 -0400

    whoops! sorry!
    accidentally removed a line

--- packages/agent/bin/agent.rs
+++ packages/agent/bin/agent.rs
@@ -76,6 +76,7 @@ async fn main() {
     /* setup logger */
     let _logs_guard = if use_ui || !args.stdout_logs {
         let log_folder = args.log_folder.unwrap_or_else(||
+            if args.stdout_logs {
                 "/var/log/playit".to_string()
             } else {
                 "./logs".to_string()
