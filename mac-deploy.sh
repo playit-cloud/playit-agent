a51e2b583f6d0249e4be0c97c96608d02e134798
Patrick Lorio <dev@plorio.com>
2022-05-22 15:28:42 -0700

    Fix mac-deploy.sh

--- mac-deploy.sh
+++ mac-deploy.sh
@@ -4,7 +4,9 @@ DEPLOY_TARGET=$1
 FOLDER=$(dirname "$0")
 REMOTE_PATH=/var/www/playit/downloads
 
+VERSION="$(toml get "${FOLDER}/packages/agent/Cargo.toml" package.version | sed "s/\"//g")"
+
 bash ${FOLDER}/build-scripts/macos-app.sh
 scp ${FOLDER}/build-scripts/out/playit.dmg ${DEPLOY_TARGET}:${REMOTE_PATH}/playit-${VERSION}.dmg
 scp ${FOLDER}/target/release/agent ${DEPLOY_TARGET}:${REMOTE_PATH}/playit-${VERSION}-apple-m1
-scp ${FOLDER}/x86_64-apple-darwin/target/release/agent ${DEPLOY_TARGET}:${REMOTE_PATH}/playit-${VERSION}-apple-intel
+scp ${FOLDER}/target/x86_64-apple-darwin/release/agent ${DEPLOY_TARGET}:${REMOTE_PATH}/playit-${VERSION}-apple-intel
