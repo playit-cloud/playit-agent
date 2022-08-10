0a439c57c7628bc9f482ef6aa80837b3d0987684
Patrick Lorio <dev@plorio.com>
2022-08-10 16:27:08 -0700

    Fix mac deploy script

--- build-scripts/macos-run.sh
+++ build-scripts/macos-run.sh
@@ -13,11 +13,11 @@ LOGS_PATH=$(pwd)
 
 if [[ $(uname -p) == 'arm' ]]; then
   osascript -e "tell app \"Terminal\"
-    do script \" ${SCRIPT_DIR}/agent-m1 --config-file=${CONFIG_PATH}/playit.toml --log-folder=${LOGS_PATH}/playit\"
+    do script \" ${SCRIPT_DIR}/agent-m1 --config-file=${CONFIG_PATH}/playit.toml\"
   end tell"
 else
   osascript -e "tell app \"Terminal\"
-    do script \" ${SCRIPT_DIR}/agent-intel --config-file=${CONFIG_PATH}/playit.toml --log-folder=${LOGS_PATH}/playit\"
+    do script \" ${SCRIPT_DIR}/agent-intel --config-file=${CONFIG_PATH}/playit.toml\"
   end tell"
 fi
 
--- mac-deploy.sh
+++ mac-deploy.sh
@@ -1,12 +1,12 @@
 # SHOULD BE RUN ON M1 MAC
 
-DEPLOY_TARGET=$1
 FOLDER=$(dirname "$0")
-REMOTE_PATH=/var/www/playit/downloads
-
 VERSION="$(toml get "${FOLDER}/packages/agent_cli/Cargo.toml" package.version | sed "s/\"//g")"
 
 bash ${FOLDER}/build-scripts/macos-app.sh
-scp ${FOLDER}/build-scripts/out/playit.dmg ${DEPLOY_TARGET}:${REMOTE_PATH}/playit-${VERSION}.dmg
-scp ${FOLDER}/target/release/agent ${DEPLOY_TARGET}:${REMOTE_PATH}/playit-${VERSION}-apple-m1
-scp ${FOLDER}/target/x86_64-apple-darwin/release/agent ${DEPLOY_TARGET}:${REMOTE_PATH}/playit-${VERSION}-apple-intel
+
+mkdir -p ${FOLDER}/build-deploy
+
+cp ${FOLDER}/build-scripts/out/playit.dmg ${FOLDER}/build-deploy/playit-${VERSION}.dmg
+cp ${FOLDER}/target/release/agent ${FOLDER}/build-deploy/playit-${VERSION}-apple-m1
+cp ${FOLDER}/target/x86_64-apple-darwin/release/agent ${FOLDER}/build-deploy/playit-${VERSION}-apple-intel
