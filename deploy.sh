965cb36d6872ffee01bef6a7eb74c52b017d3f3e
Patrick Lorio <dev@plorio.com>
2022-08-10 16:25:55 -0700

    Improve deploy script to store locally

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,16 @@
 # SHOULD BE RUN ON x86-64 linux machine
 
-DEPLOY_TARGET=$1
 FOLDER=$(dirname "$0")
-REMOTE_PATH=/var/www/playit/downloads
-
 VERSION="$(toml get "${FOLDER}/packages/agent_cli/Cargo.toml" package.version | sed "s/\"//g")"
-echo "deploying version ${VERSION} to ${DEPLOY_TARGET}:${REMOTE_PATH}"
 
 # build
 bash ${FOLDER}/build-scripts/ubuntu-deb.sh
 cross build --release --target x86_64-pc-windows-gnu --bin=agent
 cargo build --release --bin=agent
 
-# upload
-scp ${FOLDER}/target/aarch64-unknown-linux-gnu/release/agent ${DEPLOY_TARGET}:${REMOTE_PATH}/playit-${VERSION}-aarch64
-scp ${FOLDER}/target/armv7-unknown-linux-gnueabihf/release/agent ${DEPLOY_TARGET}:${REMOTE_PATH}/playit-${VERSION}-arm7
-scp ${FOLDER}/target/release/agent ${DEPLOY_TARGET}:${REMOTE_PATH}/playit-${VERSION}
-scp ${FOLDER}/target/x86_64-pc-windows-gnu/release/agent.exe ${DEPLOY_TARGET}:${REMOTE_PATH}/playit-${VERSION}.exe
+rm -f build-deploy/*
+
+cp ${FOLDER}/target/aarch64-unknown-linux-gnu/release/agent build-deploy/playit-${VERSION}-aarch64
+cp ${FOLDER}/target/armv7-unknown-linux-gnueabihf/release/agent build-deploy/playit-${VERSION}-arm7
+cp ${FOLDER}/target/release/agent build-deploy/playit-${VERSION}
+cp ${FOLDER}/target/x86_64-pc-windows-gnu/release/agent.exe build-deploy/playit-${VERSION}-unsigned.exe
