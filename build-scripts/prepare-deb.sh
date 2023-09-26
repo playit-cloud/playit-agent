1704d9c159d4640440e752f84b0d33e044a49590
Patrick Lorio <dev@plorio.com>
2023-09-26 16:12:59 -0700

    Add support for 32bit linux 0.15.1

--- build-scripts/prepare-deb.sh
+++ build-scripts/prepare-deb.sh
@@ -5,10 +5,12 @@ set -e
 cargo build --release --target=x86_64-unknown-linux-musl
 cargo build --release --target=aarch64-unknown-linux-gnu
 cargo build --release --target=armv7-unknown-linux-gnueabihf
+cargo build --release --target=i686-unknown-linux-gnu
 
 bash "${SCRIPT_DIR}/package-linux-deb.sh" x86_64-unknown-linux-musl amd64
 bash "${SCRIPT_DIR}/package-linux-deb.sh" aarch64-unknown-linux-gnu arm64
 bash "${SCRIPT_DIR}/package-linux-deb.sh" armv7-unknown-linux-gnueabihf armhf
+bash "${SCRIPT_DIR}/package-linux-deb.sh" i686-unknown-linux-gnu i386
 
 ROOT_CARGO_FILE="${SCRIPT_DIR}/../Cargo.toml"
 VERSION=$(toml get "${ROOT_CARGO_FILE}" workspace.package.version | sed "s/\"//g")
@@ -16,3 +18,4 @@ VERSION=$(toml get "${ROOT_CARGO_FILE}" workspace.package.version | sed "s/\"//g
 cp target/deb/playit_amd64.deb "${SCRIPT_DIR}/../../ppa/data/playit_${VERSION}_amd64.deb"
 cp target/deb/playit_arm64.deb "${SCRIPT_DIR}/../../ppa/data/playit_${VERSION}_arm64.deb"
 cp target/deb/playit_armhf.deb "${SCRIPT_DIR}/../../ppa/data/playit_${VERSION}_armhf.deb"
+cp target/deb/playit_i386.deb "${SCRIPT_DIR}/../../ppa/data/playit_${VERSION}_i386.deb"
