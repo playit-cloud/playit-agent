630fe84d2dc46fa056f4e81c68c1abacdfa42052
Patrick Lorio <patrick@methods.dev>
2024-08-08 09:37:15 -0700

    Use reqwest to fix docker build & bump version

--- packages/agent_core/src/api/http_client.rs
+++ packages/agent_core/src/api/http_client.rs
@@ -1,9 +1,4 @@
-use std::convert::Infallible;
-
-use bytes::{Buf, Bytes};
-use http_body_util::{combinators::BoxBody, BodyExt, Full};
-use hyper::{header, Method, Request, StatusCode};
-use hyper_util::{client::legacy::{connect::HttpConnector, Client}, rt::TokioExecutor};
+use reqwest::StatusCode;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
 
@@ -12,32 +7,15 @@ use crate::api::api::{ApiResult, PlayitHttpClient};
 pub struct HttpClient {
     api_base: String,
     auth_header: Option<String>,
-
-    client: Client<hyper_rustls::HttpsConnector<HttpConnector>, BoxBody<Bytes, Infallible>>,
+    client: reqwest::Client,
 }
 
 impl HttpClient {
     pub fn new(api_base: String, auth_header: Option<String>) -> Self {
-        let connector = if api_base.starts_with("http://") {
-            hyper_rustls::HttpsConnectorBuilder::new()
-                .with_webpki_roots()
-                .https_or_http()
-                .enable_http1()
-                .enable_http2()
-                .build()
-        } else {
-            hyper_rustls::HttpsConnectorBuilder::new()
-                .with_webpki_roots()
-                .https_only()
-                .enable_http1()
-                .enable_http2()
-                .build()
-        };
-
         HttpClient {
             api_base,
             auth_header,
-            client: Client::builder(TokioExecutor::new()).build(connector),
+            client: reqwest::Client::new(),
         }
     }
 
@@ -50,39 +28,27 @@ impl PlayitHttpClient for HttpClient {
     type Error = HttpClientError;
 
     async fn call<Req: Serialize + Send, Res: DeserializeOwned, Err: DeserializeOwned>(&self, path: &str, req: Req) -> Result<ApiResult<Res, Err>, Self::Error> {
-        let mut builder = Request::builder()
-            .uri(format!("{}{}", self.api_base, path))
-            .method(Method::POST);
+        let mut builder = self.client.post(format!("{}{}", self.api_base, path));
 
         if let Some(auth_header) = &self.auth_header {
             builder = builder.header(
-                header::AUTHORIZATION,
+                reqwest::header::AUTHORIZATION,
                 auth_header,
             );
         }
 
         let res = async move {
-            let request_str = serde_json::to_string(&req)
-                .map_err(|e| HttpClientError::SerializeError(e))?;
+            builder = builder.json(&req);
+            let request = builder.build()?;
 
-            let request = builder
-                .body(BoxBody::new(Full::new(Bytes::from(request_str))))
-                .unwrap();
-
-            let response = self.client.request(request).await
-                .map_err(|e| HttpClientError::RequestError(e))?;
+            let response = self.client.execute(request).await?;
 
             let response_status = response.status();
             if response_status == StatusCode::TOO_MANY_REQUESTS {
                 return Err(HttpClientError::TooManyRequests);
             }
 
-            let bytes = response.into_body().collect().await
-                .map_err(|e| HttpClientError::BodyReadError(e))?
-                .aggregate();
-
-            let response_txt = String::from_utf8_lossy(bytes.chunk());
-
+            let response_txt = response.text().await?;
             let result: ApiResult<Res, Err> = serde_json::from_str(&response_txt)
                 .map_err(|e| {
                     tracing::error!("failed to parse json:\n{}", response_txt);
@@ -104,7 +70,13 @@ impl PlayitHttpClient for HttpClient {
 pub enum HttpClientError {
     SerializeError(serde_json::Error),
     ParseError(serde_json::Error, StatusCode, String),
-    RequestError(hyper_util::client::legacy::Error),
-    BodyReadError(hyper::Error),
+    RequestError(reqwest::Error),
     TooManyRequests,
+}
+
+impl From<reqwest::Error> for HttpClientError {
+    fn from(value: reqwest::Error) -> Self {
+        HttpClientError::RequestError(value)
+    }
+
 }
