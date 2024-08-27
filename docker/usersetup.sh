5cc0fcd67928977a820c2ae10ac334ec5dfe7feb
kevinf100 <12779728+kevinf100@users.noreply.github.com>
2024-08-26 22:07:05 -0400

    Change default UUID and GUID to something you would manually assign

--- docker/usersetup.sh
+++ docker/usersetup.sh
@@ -2,10 +2,10 @@
 
 if ! id "playit" >/dev/null 2>&1; then
   if [ -z "${PLAYIT_UUID}" ]; then
-    PLAYIT_UUID=1000
+    PLAYIT_UUID=2000
   fi
   if [ -z "${PLAYIT_GUID}" ]; then
-    PLAYIT_GUID=1000
+    PLAYIT_GUID=2000
   fi
   addgroup -g ${PLAYIT_GUID} playit
   adduser -HSs /sbin/nologin -u ${PLAYIT_UUID} -G playit playit
